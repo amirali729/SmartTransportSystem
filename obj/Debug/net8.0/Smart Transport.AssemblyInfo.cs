34fe31f84281c14dc1d022198042d371d6734e0e
AhmadSE12 <cha91265@gmail.com>
2026-06-05 00:12:02 +0500

    add the models of our project

--- obj/Debug/net8.0/Smart Transport.AssemblyInfo.cs
+++ obj/Debug/net8.0/Smart Transport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smart Transport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3564ed0f2ae24781c42240d24e259a4359f8034c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8c195c1ff07df969ff946de594857a49a56d19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smart Transport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smart Transport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
